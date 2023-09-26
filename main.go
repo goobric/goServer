918ffc8e94d859eb22ac8c06d362c9e64fa09133
goobric <goobric@gmail.com>
2023-09-26 16:07:38 +0800

    form function

--- main.go
+++ main.go
@@ -1,2 +1,47 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+// by convention 'w' is used as the ResponseWriter and 'r' is used as the Request
+// r is pointing to the Request
+
+// form Handler function
+func formHandler(w http.ResponseWriter, r *http.Request){
+	if err := r.ParseForm(); err != nil {
+		fmt.Fprintf(w, "ParseForm() err: %v", err)
+		return
+	}
+	fmt.Fprintf(w, "POST request successful")
+	name := r.FormValue("name")
+	address := r.FormValue("address")
+	fmt.Fprintf(w, "Name = %s\n", name)
+	fmt.Fprintf(w, "Address = %s\n", address)
+}
+
+//hello Handler
+func helloHandler(w http.ResponseWriter, r *http.Request){
+	if r.URL.Path != "/hello"{
+		http.Error(w, "404 not found", http.StatusNotFound)
+		return
+	}
+	if r.Method != "GET"{
+		http.Error(w, "Method is not supported", http.StatusNotFound)
+	}
+	fmt.Fprintf(w, "hello there!")
+}
+
+func main(){
+	fileServer := http.FileServer(http.Dir("./static"))
+	http.Handle("/", fileServer)
+	http.HandleFunc("/form", formHandler)
+	http.HandleFunc("/hello", helloHandler)
+
+	fmt.Printf("Starting server at port 8080\n")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
+}
